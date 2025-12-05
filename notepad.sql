3ed6e7060006d9b430385620a026acb0845ab77d
Akash tiwari <124442437+a1official@users.noreply.github.com>
2025-12-05 19:23:30 +0530

    Update notepad.sql
    question 1 done purchase per month excluding refunds

--- notepad.sql
+++ notepad.sql
@@ -1,1 +1,18 @@
+/*question 1
+ Approach:
+First, remove all refunded transactions by keeping only the rows where refund_time is NULL.
+After filtering,group the remaining records by the year and month extracted from purchase_time,
+ and then count how many transactions fall into each year–month bucket. */
+
+/* Q1 purchases per month  */
+SELECT
+  DATE_TRUNC('month', purchase_time) AS month,
+  COUNT(*) AS purchases_count
+FROM transactions
+WHERE refund_time IS NULL
+GROUP BY DATE_TRUNC('month', purchase_time)
+ORDER BY month;
+
+
+
 
